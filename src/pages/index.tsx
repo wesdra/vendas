106af13022a4a79858b70437768ff78c2fc2fc62
wesdra <wesdralima@gmail.com>
2022-01-02 13:16:40 -0300

    Adicionando Daniel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,11 +14,11 @@ function Home() {
             { nome: 'Ednaldo', img: '/img/ednaldo.png', link: 'https://bit.ly/39WBASx', id: '39WBASx', antigo: '3jbAWED' },
             { nome: 'Taina', img: '/img/taina.png', link: 'https://bit.ly/2Y6DHkt', id: '2Y6DHkt', antigo: '2UIs0yd' },
             { nome: 'Aline', img: '/img/alinee.png', link: 'https://bit.ly/3vMpGVq', id: '3vMpGVq', antigo: '3vMpGVq' },
-            { nome: 'Marta', img: '/img/marta.png', link: 'https://bit.ly/3BOTmCJ', id: '3BOTmCJ', antigo: '3BOTmCJ' }
+            { nome: 'Marta', img: '/img/marta.png', link: 'https://bit.ly/3BOTmCJ', id: '3BOTmCJ', antigo: '3BOTmCJ' },
+            { nome: 'Daniel', img: '/img/daniel.png', link: 'https://bit.ly/3kSrZ5n', id: '3kSrZ5n', antigo: '3dn8nA0' }
         ];
         const listaoff = [
-            { nome: 'Monise', img: '/img/monise.png', link: 'https://bit.ly/3onMfOk', id: '3onMfOk', antigo: '3295waJ' },
-            { nome: 'Daniel', img: '/img/daniel.png', link: 'https://bit.ly/3kSrZ5n', id: '3kSrZ5n', antigo: '3dn8nA0' }
+            { nome: 'Monise', img: '/img/monise.png', link: 'https://bit.ly/3onMfOk', id: '3onMfOk', antigo: '3295waJ' }
         ];
         var novalista = lista.sort(function () {
             return 0.6 - Math.random();
