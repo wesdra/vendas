67e6be4ae072e0ff748b5db2a88b66be37b490d2
wesdra <wesdralima@gmail.com>
2022-10-12 11:02:13 -0300

    adicionar Rosana vendedora

--- VendedoresDb.ts
+++ VendedoresDb.ts
@@ -11,4 +11,5 @@ export const VendedoresDb = [
     { nome: 'Gustavo', img: '/luizgustavo.png', online: true, link: 'https://wa.me/message/XKSJND5ULUMKK1?utm_source=Luiz+Gustavo', id: 'XKSJND5ULUMKK1' },
     { nome: 'Rosa', img: '/rosa.png', online: true, link: 'https://wa.me/5511913195773', id: '5511913195773' },
     { nome: 'Taina', img: '/taina.png', online: true, link: 'https://wa.me/5511986127633', id: '5511986127633' },
+    { nome: 'Rosana', img: '/rosana.png', online: true, link: 'https://wa.me/5511959761773', id: '5511959761773' },
 ] 
