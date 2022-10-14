3eddd8e73d16ef00648a3c934c9ba91d3ba58c59
wesdra <wesdralima@gmail.com>
2022-10-14 13:10:14 -0300

    adicionando a Fernanda

--- VendedoresDb.ts
+++ VendedoresDb.ts
@@ -8,8 +8,9 @@ export interface VendedoresData {
 }
 
 export const VendedoresDb = [
-    { nome: 'Gustavo', img: '/luizgustavo.png', online: true, link: 'https://wa.me/message/XKSJND5ULUMKK1?utm_source=Luiz+Gustavo', id: 'XKSJND5ULUMKK1' },
-    { nome: 'Rosa', img: '/rosa.png', online: true, link: 'https://wa.me/5511913195773', id: '5511913195773' },
-    { nome: 'Taina', img: '/taina.png', online: true, link: 'https://wa.me/5511986127633', id: '5511986127633' },
-    { nome: 'Rosana', img: '/rosanab.png', online: true, link: 'https://wa.me/5511959761773', id: '5511959761773' },
+    { nome: 'Fernanda', img: '/fernanda.png', online: true, link: 'https://bit.ly/39TYFVO', id: '5Z5K5EMRKBG4G1' },
+    { nome: 'Gustavo', img: '/luizgustavo.png', online: true, link: 'https://bit.ly/37zzbPU', id: 'XKSJND5ULUMKK1' },
+    { nome: 'Rosa', img: '/rosa.png', online: true, link: 'https://bit.ly/3CV3pKa', id: '5511913195773' },
+    { nome: 'Taina', img: '/taina.png', online: true, link: 'https://bit.ly/2Y6DHkt', id: '5511986127633' },
+    { nome: 'Rosana', img: '/rosanab.png', online: true, link: 'https://bit.ly/3rRNKVH', id: '5511959761773' },
 ] 
