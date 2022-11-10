cd57bae447bb1682d61379c14670c4bf2fa5af12
wesdra <wesdralima@gmail.com>
2022-11-10 11:02:27 -0300

    vendedores

--- VendedoresDb.ts
+++ VendedoresDb.ts
@@ -9,8 +9,8 @@ export interface VendedoresData {
 
 export const VendedoresDb = [
     //{ nome: 'Fernanda', img: '/fernanda.png', online: true, link: 'https://bit.ly/39TYFVO', id: '5Z5K5EMRKBG4G1' },
-    { nome: 'Gustavo', img: '/luizgustavo.png', online: true, link: 'https://bit.ly/37zzbPU', id: 'XKSJND5ULUMKK1' },
+//{ nome: 'Gustavo', img: '/luizgustavo.png', online: true, link: 'https://bit.ly/37zzbPU', id: 'XKSJND5ULUMKK1' },
     { nome: 'Rosa', img: '/rosa.png', online: true, link: 'https://bit.ly/3CV3pKa', id: '5511913195773' },
-    { nome: 'Taina', img: '/taina.png', online: true, link: 'https://bit.ly/2Y6DHkt', id: '5511986127633' },
+  //  { nome: 'Taina', img: '/taina.png', online: true, link: 'https://bit.ly/2Y6DHkt', id: '5511986127633' },
     { nome: 'Rosana', img: '/rosanab.png', online: true, link: 'https://bit.ly/3rRNKVH', id: '5511959761773' },
 ] 
