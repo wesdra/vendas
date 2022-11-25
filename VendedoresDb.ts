2a9b34c98d2711889191dd216c6ec8df48af1345
wesdra <wesdralima@gmail.com>
2022-11-25 16:13:11 -0300

    remover rosana

--- VendedoresDb.ts
+++ VendedoresDb.ts
@@ -14,14 +14,17 @@ export const VendedoresDb = [
   //Rosa https://bit.ly/3CV3pKa
   //Rosana https://bit.ly/3rRNKVH
     { nome: 'Atendente 01', img: '/avatar1.jpg', online: true, link: 'https://bit.ly/3CV3pKa', id: '5511913195773' },
-    { nome: 'Atendente 02', img: '/avatar2.jpg', online: true, link: 'https://bit.ly/3rRNKVH', id: '5511959761773' },
+   // { nome: 'Atendente 02', img: '/avatar2.jpg', online: true, link: 'https://bit.ly/3rRNKVH', id: '5511959761773' },
+   { nome: 'Atendente 02', img: '/avatar7.jpg', online: true, link: 'https://bit.ly/39WBASx', id: '5511958516111' },
 
     { nome: 'Atendente 03', img: '/avatar3.jpg', online: true, link: 'https://bit.ly/3CV3pKa', id: '5511913195773' },
-    { nome: 'Atendente 04', img: '/avatar4.jpg', online: true, link: 'https://bit.ly/3rRNKVH', id: '5511959761773' },
+    { nome: 'Atendente 04', img: '/avatar8.jpg', online: true, link: 'https://bit.ly/39WBASx', id: '5511958516111' },
+    //{ nome: 'Atendente 04', img: '/avatar4.jpg', online: true, link: 'https://bit.ly/3rRNKVH', id: '5511959761773' },
 
     { nome: 'Atendente 05', img: '/avatar5.jpg', online: true, link: 'https://bit.ly/3CV3pKa', id: '5511913195773' },
-    { nome: 'Atendente 06', img: '/avatar6.jpg', online: true, link: 'https://bit.ly/3rRNKVH', id: '5511959761773' },
+    { nome: 'Atendente 06', img: '/avatar10.jpg', online: true, link: 'https://bit.ly/39WBASx', id: '5511958516111' },
+  //  { nome: 'Atendente 06', img: '/avatar6.jpg', online: true, link: 'https://bit.ly/3rRNKVH', id: '5511959761773' },
 
-    { nome: 'Atendente 07', img: '/avatar7.jpg', online: true, link: 'https://bit.ly/39WBASx', id: '5511958516111' },
-    { nome: 'Atendente 08', img: '/avatar8.jpg', online: true, link: 'https://bit.ly/39WBASx', id: '5511958516111' },
+   
+   
 ] 
